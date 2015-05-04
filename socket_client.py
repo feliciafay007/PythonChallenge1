bdc9d8d4b2b13414359b3213da0bc560b0bbec76
feliciafay <feliciafay@feliciafays-MacBook-Pro.local>
2015-05-04 00:52:20 -0700

    initial submit for milestone 1

--- socket_client.py
+++ socket_client.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python 
+
 import socket
+
 TCP_IP = '127.0.0.1'
 TCP_PORT = 9876
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!" 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
-print "received data:", data
+
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_client.connect((TCP_IP, TCP_PORT))
+socket_client.send(MESSAGE)
+data = socket_client.recv(BUFFER_SIZE)
+socket_client .close()
+print "client received data from server:", data
+
